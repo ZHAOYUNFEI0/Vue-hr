f366199ed3c01e7e1b35ecb19b9b33e2ccb7dbc2
zhaoyunfei <zyf120600@163.com>
2023-03-23 18:08:53 +0800

    完成完工管理：排序功能，分页功能

--- src/views/employees/employees.vue
+++ src/views/employees/employees.vue
@@ -16,14 +16,14 @@
         <el-table :data="employees" border>
           <el-table-column label="序号" type="index" />
           <el-table-column label="姓名" prop="username" />
-          <el-table-column label="工号" prop="workNumber" />
+          <el-table-column sortable label="工号" prop="workNumber" />
           <el-table-column label="聘用形式">
             <template v-slot="scop">
               {{ meiju(scop.row.formOfEmployment) }}
             </template>
           </el-table-column>
           <el-table-column label="部门" prop="departmentName" />
-          <el-table-column label="入职时间" prop="timeOfEntry" />
+          <el-table-column sortable label="入职时间" prop="timeOfEntry" />
           <el-table-column label="操作" width="280">
             <template>
               <el-button type="text" size="small">查看</el-button>
@@ -34,7 +34,15 @@
         </el-table>
         <!-- 分页组件 -->
         <el-row type="flex" justify="center" align="middle" style="height: 60px">
-          <el-pagination layout="prev, pager, next" />
+          <el-pagination
+            :current-page="q.page"
+            :page-sizes="[2,3, 4, 5]"
+            :page-size="q.size"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="total"
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+          />
         </el-row>
       </el-card>
     </div>
@@ -74,6 +82,16 @@ export default {
       const i = employees.hireType.find(item => item.id === val)
       // console.log(i)
       return i.value
+    },
+    handleSizeChange(val) {
+      // console.log(val)
+      this.q.size = val
+      this.loadEmployees()
+    },
+    handleCurrentChange(val) {
+      // console.log(val)
+      this.q.page = val
+      this.loadEmployees()
     }
   }
 }
